b6dec1246461b9d12a9e87eeba9f3ea897eeacea
AlbaCastilla <castillamacki@gmail.com>
2025-03-09 19:46:43 +0100

    incorporo ventas al routes

--- sge-odoo/src/routes.js
+++ sge-odoo/src/routes.js
@@ -3,9 +3,11 @@
 import Home from './components/Estructura.svelte';
 import EmpleadosVista from './components/empleados/EmpleadosVista.svelte';
 import InventarioVista from './components/inventario/InventarioVista.svelte';
+import VentasVista from './components/ventas/ventasVista.svelte'
 
 export default{
     '/': Home,
     '/empleados': EmpleadosVista,
-    '/inventario': InventarioVista
+    '/inventario': InventarioVista,
+    '/ventas': VentasVista
 };
